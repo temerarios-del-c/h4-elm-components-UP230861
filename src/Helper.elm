cc47c70515db04570b80cd443f031af470e9ea03
axel alonso <up230861@alumnos.upa.edu.mx>
2026-02-22 18:32:03 -0600

    archivocompleto1

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,11 +1,45 @@
 module Helper exposing (..)
 
-import Html
-import Html.Attributes
+import Char
+import Html exposing (Html, a, div, h1, h2, h3, h4, h5, h6, text)
+import Html.Attributes exposing (href)
 
 
+joinWords : String -> String -> String
+joinWords word1 word2 =
+    word1 ++ word2
 
--- Puedes usar una definición como la siguiente para probar y visualizar tus resultados, solo debes definir "headers" y "hyperlink"
---main : Html.Html msg
---main =
---    Html.div [] [ headers "Titulos", hyperlink "https://upa.edu.mx" "My School" ]
+
+isUpperChars : List Char -> List Bool
+isUpperChars list =
+    List.map Char.isUpper list
+
+
+evalChars : List Char -> (Char -> Bool) -> List Bool
+evalChars list funTrans =
+    List.map funTrans list
+
+
+headers : String -> Html msg
+headers param =
+    div []
+        [ h1 [] [ text param ]
+        , h2 [] [ text param ]
+        , h3 [] [ text param ]
+        , h4 [] [ text param ]
+        , h5 [] [ text param ]
+        , h6 [] [ text param ]
+        ]
+
+
+hyperlink : String -> String -> Html msg
+hyperlink url txt =
+    a [ href url ] [ text txt ]
+
+
+main : Html msg
+main =
+    div []
+        [ headers "Hello Elm"
+        , hyperlink "https://elm-lang.org" "Go to Elm"
+        ]
